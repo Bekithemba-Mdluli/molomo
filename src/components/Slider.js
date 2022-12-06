83e22a7a3f339367815c9d4b39f195c381278ae6
Bekithemba Mdluli <putthetrust.gamedev@gmail.com>
2022-12-06 21:23:36 +0200

    Added home rem

---
+++ src/components/Slider.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./Slider.css";
+
+function Slider() {
+  return (
+    <div className="slider">
+      <ol className="slider__list">
+        <li className="active"></li>
+        <li></li>
+        <li></li>
+      </ol>
+    </div>
+  );
+}
+
+export default Slider;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Header from "../components/Header";
 import Panel from "../components/Panel";
+import Slider from "../components/Slider";
 
+import "./Home.css";
 function Home() {
   return (
-    <div>
+    <div className="home">
       <Header />
       <Panel />
+      <div className="home__title">
+        <h1>A Catchy Title typed over multiple lines</h1>
+      </div>
+      <Slider />
     </div>
   );
 }
