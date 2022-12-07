5af6b551e6c5cd5c8736bc9c213112054a20016f
Bekithemba Mdluli <putthetrust.gamedev@gmail.com>
2022-12-07 07:55:43 +0200

    Added Details and Services rem

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,24 @@
 import React from "react";
+import Details from "../components/Details";
 import Header from "../components/Header";
 import Panel from "../components/Panel";
+import Services from "../components/Services";
 import Slider from "../components/Slider";
 
 import "./Home.css";
 function Home() {
   return (
     <div className="home">
-      <Header />
-      <Panel />
-      <div className="home__title">
-        <h1>A Catchy Title typed over multiple lines</h1>
+      <div className="hero">
+        <Header />
+        <Panel />
+        <div className="home__title">
+          <h1>A Catchy Title typed over multiple lines</h1>
+        </div>
+        <Slider />
       </div>
-      <Slider />
+      <Details />
+      <Services />
     </div>
   );
 }
