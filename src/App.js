e7d0425265abe548de691cfc060f6c524fedf321
Bekithemba <putthetrust.gamedev@gmail.com>
2022-12-08 15:32:07 +0200

    Fixed gift branch structure Co-authored-by: austingift24@gmail.com

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import React from 'react';
+
+import About from './components/About.js';
+
+//Used Bheki's code below on Header.js ;)
+import Header from './components/Header.js';
+
 function App() {
   return (
-    <div>
-      <h1>Molomo</h1>
+    <div className="App">
+      <Header />
+      <About />
     </div>
   );
 }
