97047acc195635a02ae6cc9c3b9b0850a3612f7d
Vii Hunnid <viihunnid@icloud.com>
2022-12-14 01:06:47 +0200

    Fixed the footer and the products

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,30 +1,36 @@
 import "./FooterStyles.css";
-import socialIcons from "./images/icons/socialIcons.png";
+import facebook from "./images/icons/icon-facebook.png";
+import instagram from "./images/icons/icon-instagram.png";
+import linkedIn from "./images/icons/icon-linkedin.png";
 
 const Footer = () => {
   return (
-    <>
-      <div className="footer">
-
-        <div className="bottom">
-          <div>
-            <a>Copytight &copy; website owner all rights reserved</a>
-            <p className="creators">Created by Isaac Gift and Themba</p>
-          </div>
-          <div>
-            <a href="https://www.krostshelving.com/">Powered by — KROST SHELVING AND RACKING</a>
-
-          </div>
-          <div>
-            <a href="index.html">
-              <img src={socialIcons} alt="My logo" width="100%" height="90%" viewBox="0 0 10 10" className="ccustom" fill="0A003C" object-fit="contain">
-              </img>
-            </a>          
-          </div>
-        </div>
+    <div className="footer">
+      {/* <div className="bottom"> */}
+      <div>
+        <a>Copytight &copy; website owner all rights reserved</a>
+        <p className="creators">Created by Isaac, Gift and Themba</p>
       </div>
-    </>
+      <div>
+        <a href="https://www.krostshelving.com/">
+          Powered by — KROST SHELVING AND RACKING
+        </a>
+      </div>
+      <div className="footer__icons">
+      <a href="https://www.instagram.com/">
+          <img src={instagram} alt="instagram icon" />
+        </a>
+        <a href="https://www.facebook.com/">
+          <img src={facebook} alt="facebook icon" />
+        </a>
+        <a href="https://www.linkedin.com/">
+          <img src={linkedIn} alt="linked in icon" />
+        </a>
+      </div>
+    </div>
+    // </div>
   );
 };
 
 export default Footer;
+
--- src/components/ProductTabs.js
+++ src/components/ProductTabs.js
@@ -1,8 +1,21 @@
+import React, { useState } from 'react'
+
 import "./ProductTabs.css";
+import Modal from './Modal'
 import pic1 from './images/pic1.jpg';
 import pic2 from './images/pic2.jpg';
+import pic3 from './images/pic1.jpg';
+import pic4 from './images/pic2.jpg';
+import pic5 from './images/pic2.jpg';
+import pic6 from './images/pic2.jpg';
 
   const ProductTabs = () => {
+    const [show, setShow] = useState(false);
+
+    const foo = () => {
+      setShow(!show)
+    }
+
     return (
       <>
         <section class="services">
@@ -15,33 +28,93 @@ import pic2 from './images/pic2.jpg';
               <div class="row">
                 <div class="service-items">
                     <div class="row">
+                      <div class="item">
+                        <div className="item-inner">
+                          <img className="item-inner" src={pic2} alt="img" />
+                          <h3 className="productDescription">Product 1</h3>
+                            <button onClick={foo} className="btn" type="button">Read more?</button>
+                              {/* MODAL */}
+                              <div className={show === true? 'show': 'hide'}>
+                                <div class="readMore">
+                                  <div className="readMoreConentImg">
+                                    <img className="readMoreConentImg" src={pic2} alt="img" />
+                                  </div>
+                                  <div className="readMoreConentDes">
+                                    <h1>
+                                      Product 1
+                                    </h1>
+                                    <p className="readMoreConentDes"> 
+                                      sectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. 
+                                      Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. 
+                                      Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
+                                      Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. 
+                                      Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus.                                
+                                    </p>                              
+                                  </div>
+                              </div>
+                            </div>
+                        </div>
 
-                    <div class="item">
                         <div className="item-inner">
                           <img className="item-inner" src={pic1} alt="img" />
-                          <h3 className="productDescription">Product 1</h3>
-                          {/* onClick={()=>{ alert('alert'); }} */}
-                          <button className="btn" type="button">Read more?</button>                              
-                          <div class="read-more-cont">
-                              {/* href="url" */}                             
-                                <img className="readMoreConent" src={pic1} alt="img" />
-                                <img className="readMoreConent" src={pic2} alt="img" />
-                                <span className="close-button"> &times;</span>
-                                <p className="readMoreConent"> 
-                                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed eiusm.
-                                </p>   
-                          </div>
+                          <h3 className="productDescription">Product 2</h3>
+                            <button onClick={foo} className="btn" type="button">Read more?</button>
+                              {/* MODAL */}
+                              <div className={show === true? 'show': 'hide'}>
+                                <div class="readMore">
+                                  <div className="readMoreConentImg">
+                                    <img className="readMoreConentImg" src={pic1} alt="img" />
+                                  </div>
+                                  <div className="readMoreConentDes">
+                                    <h1>
+                                      Product 2
+                                    </h1>
+                                    <p className="readMoreConentDes"> 
+                                      sectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. 
+                                      Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. 
+                                      Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
+                                      Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. 
+                                      Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus.                                
+                                    </p>                              
+                                  </div>
+                              </div>
+                            </div>
                         </div>
-                    </div>
 
+                        <div className="item-inner">
+                          <img className="item-inner" src={pic3} alt="img" />
+                          <h3 className="productDescription">Product 3</h3>
+                            <button onClick={foo} className="btn" type="button">Read more?</button>
+                              {/* MODAL */}
+                              <div className={show === true? 'show': 'hide'}>
+                                <div class="readMore">
+                                  <div className="readMoreConentImg">
+                                    <img className="readMoreConentImg" src={pic3} alt="img" />
+                                  </div>
+                                  <div className="readMoreConentDes">
+                                    <h1>
+                                      Product 3
+                                    </h1>
+                                    <p className="readMoreConentDes"> 
+                                      sectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. 
+                                      Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. 
+                                      Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
+                                      Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. 
+                                      Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus.                                
+                                    </p>                              
+                                  </div>
+                              </div>
+                            </div>
+                        </div>
                     </div>
                   </div>
                 </div>
             </div>
+          </div>
         </section>
       </>
     );
   };
   
   export default ProductTabs;
-  
+
