2a228011eb7124e956d944ff9c91db389f8c721b
Bekithemba <putthetrust.gamedev@gmail.com>
2023-01-09 19:33:07 +0200

    Optomized product images

--- src/components/Product.js
+++ src/components/Product.js
@@ -8,9 +8,6 @@ function Product({ name, image, url }) {
         backgroundImage: `linear-gradient( rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3) ), url(${image})`,
       }}
     >
-      {/* background-image: linear-gradient(rgba(0, 0, 0, 0.527),rgba(0, 0, 0, 0.5)) , url(EauDp1hUcAAE-bK.jpg); */}
-      {/*            `linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(${image})` */}
-      {/* background: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url('landingpagepic.jpg'); */}
       <div className="product__content">
         <h2>{name}</h2>
         <a href={url}>Read More</a>
