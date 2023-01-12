5321f43ff5e573934935a96c388359a58725604a
Bekithemba <putthetrust.gamedev@gmail.com>
2023-01-12 19:13:06 +0200

    Added image to details

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import placeholder from "../images/placeholder.webp";
+import placeholder from "../images/aboutUs.webp";
 
 import "./Details.css";
 
